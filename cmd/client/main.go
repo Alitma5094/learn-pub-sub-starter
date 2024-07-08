dece6ba5398999b99e2a4ac40bcc67cdd9511b11
Andrew Litman <litmanandrew@gmail.com>
2024-07-08 10:25:13 -0400

    Connect client to message server

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+    "os"
+    "os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+
+const CONNECT_URL = "amqp://guest:guest@localhost:5672/"
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	connection, err := amqp.Dial(CONNECT_URL)
+	if err != nil {
+		fmt.Printf("Connection: %s\n", err)
+		return
+	}
+	defer connection.Close()
+
+    username, err := gamelogic.ClientWelcome()
+    if err != nil {
+        fmt.Println(err)
+        return 
+    }
+    
+    _, _, err = pubsub.DeclareAndBind(connection, routing.ExchangePerilDirect, fmt.Sprint(routing.PauseKey, ".", username), routing.PauseKey, int(amqp.Transient))
+    if err != nil {
+        fmt.Println(err)
+        return 
+    }
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
 }
