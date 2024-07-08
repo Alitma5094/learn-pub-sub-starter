461399b9000ee508d3b86d404a4b0ce059fea1e5
Andrew Litman <litmanandrew@gmail.com>
2024-07-08 08:40:43 -0400

    Send JSON messages to message server.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -15,12 +17,23 @@ func main() {
 
 	connection, err := amqp.Dial(CONNECT_URL)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Connection: %s\n", err)
 		return
 	}
 	defer connection.Close()
 
 	fmt.Println("Successful connected to RabbitMQ server")
+
+	channel, err := connection.Channel()
+	if err != nil {
+		fmt.Printf("Channel: %s\n", err)
+		return
+	}
+
+	err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
